9b0ee3abf25dff4e579430e471695b14eedec91d
Yasin ULUSOY <yasin.ulusoy@yahoo.com>
2023-03-05 01:41:15 +0300

    home view created

---
+++ src/views/home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Slider from '../../components/slider';
+
+
+
+
+function Home() {
+
+    
+    return (
+        <div className='container'>
+            <Slider/>
+        </div>
+    );
+}
+
+export default Home;
